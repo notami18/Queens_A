2505e4607ff104b942b40ce53650c429fa1f8ffa
Andrés notami <andresnotami@gmail.com>
2016-05-15 21:03:49 -0500

    Init
    Iniciando project

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name queensAApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the queensAApp
+ */
+angular.module('queensAApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name queensAApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the queensAApp
+ */
+angular.module('queensAApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
